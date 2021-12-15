1dfe9f40a393c8d7d9ac17f02b166a5e003c46b9
AndreyGladkov <andrewgladkovlz@gmail.com>
2021-12-15 18:21:54 +0300

    HH-142141 Off react-jsx rule

--- base.js
+++ base.js
@@ -886,6 +886,8 @@ module.exports = {
         ],
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'error',
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
     },
     parser: '@babel/eslint-parser',
     parserOptions: {
